75f9b4c856cd985ef694f3dca92c03ecccdbbf81
limz <limzgiser@163.com>
2020-11-11 17:17:20 +0800

    添加登陆页面

--- Source/components/controls/MainBarControl/geo3d/userInfo.vue
+++ Source/components/controls/MainBarControl/geo3d/userInfo.vue
@@ -1,69 +1,69 @@
 <template>
-
-        <div class="login-info-pancel" >
-            <span class="login-g1">当前用户：管理员</span>
-            <span class="login-g2 exit">退出</span>
-            <div class="login-g2 full-screen" @click="switchFullScreen()">
-               <img :src="require('../../../../images/geo3d/icon_fullscreen.png')"/>
-            </div>
-         
-        </div>
-
+<div class="login-info-pancel">
+    <span class="login-g1">当前用户：管理员</span>
+    <span class="login-g2 exit" @click="exitlogin()">退出</span>
+    <div class="login-g2 full-screen" @click="switchFullScreen()">
+        <img :src="require('../../../../images/geo3d/icon_fullscreen.png')" />
+    </div>
+</div>
 </template>
 
 <script>
-  import screenfull from 'screenfull'
- 
+import screenfull from "screenfull";
+
 export default {
-  data() {
-    return {
-        isFullscreen:true,
-    };
-  },
-  created() {},
-  mounted() {
-
-  },
-  computed: {},
-  methods: {
-
-   switchFullScreen () {
-      screenfull.toggle();
- 
-  },
-  },
-  beforeDestroy() {
-    
-  }
+    data() {
+        return {
+            isFullscreen: true
+        };
+    },
+    created() {},
+    mounted() {},
+    computed: {},
+    methods: {
+        exitlogin() {
+            window.location.href = "./login.html";
+        },
+        switchFullScreen() {
+            screenfull.toggle();
+        }
+    },
+    beforeDestroy() {}
 };
 </script>
 
 <style scoped>
-.login-info-pancel{
-        width:100%;
-        height:100%;
-        display:flex;
-        align-items:center;
-        justify-content:space-around;
-    }
-    .login-info-pancel >span{
-        margin-left:10px;
-    }
-    .login-g1{
-        color:#b3b3b3;
-        margin-right:10px;
-    }
-    .exit{
-        margin-left:15px;
-    }
-    .login-g2{
-        color:#fff;
-    }
-    .exit{
-        cursor:pointer;
-    }
-    .full-screen{
-        cursor:pointer;
-        margin-left:10px;
-    }
+.login-info-pancel {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+}
+
+.login-info-pancel>span {
+    margin-left: 10px;
+}
+
+.login-g1 {
+    color: #b3b3b3;
+    margin-right: 10px;
+}
+
+.exit {
+    margin-left: 15px;
+}
+
+.login-g2 {
+    color: #fff;
+}
+
+.exit {
+    cursor: pointer;
+}
+
+.full-screen {
+    cursor: pointer;
+    margin-left: 10px;
+}
 </style>
